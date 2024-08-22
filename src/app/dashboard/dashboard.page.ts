c84b48a9472176b0a86aa3c6c0d882fb8f0c3993
Caroline <wanguic49@gmail.com>
2024-08-22 10:27:01 +0300

    few changes made

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonGrid,IonRow,IonCol ,IonCard,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent} from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,IonGrid,IonRow,IonCol ,IonCard,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent, IonItem, IonIcon, IonLabel, IonNote ,IonList, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonGrid,IonRow,IonCol,IonCard,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent]
+  imports: [IonButton, IonNote, IonLabel, IonIcon, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonGrid,IonRow,IonCol,IonCard,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonList]
 })
 export class DashboardPage  {
 
