d5f5d618ee9599250fc7fac45cc3e3ef5f69095d
Caroline <wanguic49@gmail.com>
2024-08-23 11:23:08 +0300

    few changes made

--- src/app/contracts/contracts.page.ts
+++ src/app/contracts/contracts.page.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol,IonRow, IonCard ,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon, IonList, IonItem } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-contracts',
   templateUrl: './contracts.page.html',
   styleUrls: ['./contracts.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonItem, IonList, IonCard, IonCol, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule ,IonRow ,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon]
 })
-export class ContractsPage implements OnInit {
+export class ContractsPage  {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+
 
 }
--- src/app/offers/offers.page.ts
+++ src/app/offers/offers.page.ts
@@ -1,20 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol,IonRow, IonCard ,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon, IonList, IonItem} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
   styleUrls: ['./offers.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol,IonRow, IonCard ,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon, IonList, IonItem,CommonModule,FormsModule]
 })
-export class OffersPage implements OnInit {
+export class OffersPage  {
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
