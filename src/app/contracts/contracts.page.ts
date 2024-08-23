551b2470f6fba87d163417f347817ebf0313a30a
Caroline <wanguic49@gmail.com>
2024-08-23 15:05:28 +0300

    few changes made

--- src/app/contracts/contracts.page.ts
+++ src/app/contracts/contracts.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol,IonRow, IonCard ,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon, IonList, IonItem } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol,IonRow, IonCard ,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon, IonList, IonItem,IonButton,IonLabel,IonNote } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-contracts',
   templateUrl: './contracts.page.html',
   styleUrls: ['./contracts.page.scss'],
   standalone: true,
-  imports: [IonItem, IonList, IonCard, IonCol, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule ,IonRow ,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon]
+  imports: [IonItem, IonList, IonCard, IonCol, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule ,IonRow ,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon,IonButton,IonLabel,IonNote]
 })
 export class ContractsPage  {
 
--- src/app/documents/documents.page.ts
+++ src/app/documents/documents.page.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {  IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol,IonRow, IonCard ,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon, IonList, IonItem,IonLabel,IonNote, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-documents',
   templateUrl: './documents.page.html',
   styleUrls: ['./documents.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton,  IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol,IonRow, IonCard ,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon, IonList, IonItem,FormsModule,CommonModule,IonLabel,IonNote]
 })
-export class DocumentsPage implements OnInit {
+export class DocumentsPage  {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+ 
 
 }
