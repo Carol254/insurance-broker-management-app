c4a29da85f5ad3fc29ec1a6918128db98d727282
Caroline <wanguic49@gmail.com>
2024-08-23 10:29:43 +0300

    few changes made

--- src/app/personal-data/personal-data.page.ts
+++ src/app/personal-data/personal-data.page.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCol, IonGrid, IonRow,IonCard,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon, IonButton, IonList, IonItem } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-personal-data',
   templateUrl: './personal-data.page.html',
   styleUrls: ['./personal-data.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonItem, IonList, IonButton, IonRow, IonGrid, IonCol, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonCard,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon]
 })
-export class PersonalDataPage implements OnInit {
+export class PersonalDataPage  {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  
 
 }
