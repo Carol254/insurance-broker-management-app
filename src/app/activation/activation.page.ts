12bcd628fa9ccad29aec6e8ddaefa4f8b8bcaf43
Caroline <wanguic49@gmail.com>
2024-08-26 14:47:36 +0300

    few changes made

--- src/app/activation/activation.page.ts
+++ src/app/activation/activation.page.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonGrid, IonCol,IonRow, IonCard ,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon, IonList, IonItem,IonButton,IonLabel,IonNote } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-activation',
   templateUrl: './activation.page.html',
   styleUrls: ['./activation.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonItem, IonList, IonCard, IonCol, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule ,IonRow ,IonCardHeader,IonCardSubtitle,IonCardTitle,IonCardContent,IonIcon,IonButton,IonLabel,IonNote]
 })
-export class ActivationPage implements OnInit {
+export class ActivationPage  {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+
 
 }
